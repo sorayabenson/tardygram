c573996aaaa558d85fac94813693d7ead0f0c133
Soraya Benson <soraya.benson@gmail.com>
2021-04-15 11:58:54 -0700

    test setup for posts

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,46 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
-describe('tardygram routes', () => {
+describe('posts routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  let users;
+  beforeAll(async done => {
+    const profile = {
+      userName: 'testUser',
+      photoUrl: 'testUserPhoto.com'
+    }
+    users = await User.insert(profile);
+    
+    // token = signInData.body.token;
+
+    console.log('users', users)
+    // console.log('beforeAll', token);
+    return done;
+  })
+
+  it('posts a new tardypost', () => {
+    const newPost = {
+      user_name: 'testUser',
+      photo_url: 'testphoto.com',
+      caption: 'this is a test'
+    }
+
+    return request(app)
+      .post('/api/v1/auth/posts')
+      .set('Authorization', token)
+      .send(newPost)
+      .then(console.log('test', token))
+      .then((res) => {
+        expect(res.body).toEqual({
+          ...newPost,
+          id: '1'
+        })
+      })
+  })
+
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,13 @@
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users, posts CASCADE;
 
 CREATE TABLE users (
     github_username TEXT NOT NULL PRIMARY KEY,
     github_photo_url TEXT NOT NULL
 );
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_name TEXT NOT NULL REFERENCES users(github_username),
+    photo_url TEXT NOT NULL,
+    caption TEXT,
+    tags TEXT[]
+)
